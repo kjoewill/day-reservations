79784c768ff6aa34c4a2b173185145156d11a67c
Kevin Williams <kjoewill@gmail.com>
2023-05-30 16:22:00 -0600

    Basic system tests working

---
+++ test/system/reservations_test.rb
@@ -0,0 +1,37 @@
+require "application_system_test_case"
+
+class ReservationsTest < ApplicationSystemTestCase
+
+  test "Reserve a time for G2" do
+
+    visit root_path
+    fill_in "datepicker", with: "09-27-2100"
+    click_button "Go"
+    assert_text "09/27/2100"
+
+    find('th', text: 'Glider-2').click_link('Reserve')
+
+    assert_selector "h1", text: "High Flights Soaring", wait: 5
+
+    # CSS selector to find the first input field
+    input_field = all("input[name^='day_schedule[reservations]'][name$='[description]']").first
+    input_field.fill_in(with: '08:30, G2, 09-27-2100')
+
+    within('form') do
+      click_button 'Save'
+    end
+    assert_current_path(root_path(datepicker: Date.new(2100, 9, 27)))
+
+     # Assert that the specific cell contains the expected text
+     within "table" do
+      cell_text = find("tr:nth-child(1) td:nth-child(3)").text
+      assert_equal "08:30, G2, 09-27-2100", cell_text
+
+      cell_text = find("tr:nth-child(2) td:nth-child(3)").text
+      assert_equal "", cell_text
+    end
+
+    take_screenshot
+  end
+
+end
