c71dd8ce1dd14464dff4964d2a70b179555eafd3
Kevin Williams <kjoewill@gmail.com>
2023-05-30 14:42:35 -0600

    modified seeds and improved the home page system test

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,19 +17,46 @@ assets = [
 Asset.create(assets)
 
 # Find the asset "Glider-2"
+glider_1 = Asset.find_by(name: 'Glider-1')
 glider_2 = Asset.find_by(name: 'Glider-2')
+glider_3 = Asset.find_by(name: 'Glider-3')
+glider_4 = Asset.find_by(name: 'Glider-4')
 
 # Create day schedule associated with "Glider-2"
-day_schedule = glider_2.day_schedules.create(day: Date.new(2025, 9, 27))
+g1_day_schedule = glider_1.day_schedules.create(day: Date.new(2025, 9, 27))
+g2_day_schedule = glider_2.day_schedules.create(day: Date.new(2025, 9, 27))
+g3_day_schedule = glider_3.day_schedules.create(day: Date.new(2025, 9, 27))
+g4_day_schedule = glider_4.day_schedules.create(day: Date.new(2025, 9, 27))
 
 # Create reservations
 reservations = [
-  { time_slot: '08:30', description: '08:30, G2, 09-27-25', day_schedule: day_schedule },
-  { time_slot: '09:00', description: '09:00, G2, 09-27-25', day_schedule: day_schedule },
-  { time_slot: '09:30', description: '09:30, G2, 09-27-25', day_schedule: day_schedule },
-  { time_slot: '10:00', description: '10:00, G2, 09-27-25', day_schedule: day_schedule },
-  { time_slot: '10:30', description: '10:30, G2, 09-27-25', day_schedule: day_schedule },
-  { time_slot: '11:00', description: '11:00, G2, 09-27-25', day_schedule: day_schedule }
+  { time_slot: '08:30', description: '08:30, G1, 09-27-25', day_schedule: g1_day_schedule },
+  { time_slot: '09:00', description: '09:00, G1, 09-27-25', day_schedule: g1_day_schedule },
+  { time_slot: '09:30', description: '09:30, G1, 09-27-25', day_schedule: g1_day_schedule },
+  { time_slot: '10:00', description: '10:00, G1, 09-27-25', day_schedule: g1_day_schedule },
+  { time_slot: '10:30', description: '10:30, G1, 09-27-25', day_schedule: g1_day_schedule },
+  { time_slot: '11:00', description: '11:00, G1, 09-27-25', day_schedule: g1_day_schedule },
+  
+  { time_slot: '08:30', description: '08:30, G2, 09-27-25', day_schedule: g2_day_schedule },
+  { time_slot: '09:00', description: '09:00, G2, 09-27-25', day_schedule: g2_day_schedule },
+  { time_slot: '09:30', description: '09:30, G2, 09-27-25', day_schedule: g2_day_schedule },
+  { time_slot: '10:00', description: '10:00, G2, 09-27-25', day_schedule: g2_day_schedule },
+  { time_slot: '10:30', description: '10:30, G2, 09-27-25', day_schedule: g2_day_schedule },
+  { time_slot: '11:00', description: '11:00, G2, 09-27-25', day_schedule: g2_day_schedule },
+
+  { time_slot: '08:30', description: '08:30, G2, 09-27-25', day_schedule: g3_day_schedule },
+  { time_slot: '09:00', description: '09:00, G2, 09-27-25', day_schedule: g3_day_schedule },
+  { time_slot: '09:30', description: '09:30, G2, 09-27-25', day_schedule: g3_day_schedule },
+  { time_slot: '10:00', description: '10:00, G2, 09-27-25', day_schedule: g3_day_schedule },
+  { time_slot: '10:30', description: '10:30, G2, 09-27-25', day_schedule: g3_day_schedule },
+  { time_slot: '11:00', description: '11:00, G2, 09-27-25', day_schedule: g3_day_schedule },
+
+  { time_slot: '08:30', description: '08:30, G2, 09-27-25', day_schedule: g4_day_schedule },
+  { time_slot: '09:00', description: '09:00, G2, 09-27-25', day_schedule: g4_day_schedule },
+  { time_slot: '09:30', description: '09:30, G2, 09-27-25', day_schedule: g4_day_schedule },
+  { time_slot: '10:00', description: '10:00, G2, 09-27-25', day_schedule: g4_day_schedule },
+  { time_slot: '10:30', description: '10:30, G2, 09-27-25', day_schedule: g4_day_schedule },
+  { time_slot: '11:00', description: '11:00, G2, 09-27-25', day_schedule: g4_day_schedule }
 ]
 
 Reservation.create(reservations)
--- test/system/homes_test.rb
+++ test/system/homes_test.rb
@@ -4,7 +4,7 @@ class HomesTest < ApplicationSystemTestCase
 
   test "viewing the home page" do
     visit root_path
-    assert_selector "h1", text: "High Flights Soaring"
+    assert_selector "h1", text: "High Flights Soaring", wait: 5
   end 
 
   test "Select new date at hit go" do
@@ -19,10 +19,30 @@ class HomesTest < ApplicationSystemTestCase
     fill_in "datepicker", with: "09-27-2025"
     click_button "Go"
 
+    #Using this as a delay to avoid testing contents before they have a chance to load
+    assert_selector "h1", text: "High Flights Soaring", wait: 5
+
+    #take_screenshot
+
     # Assert that the specific cell contains the expected text
     within "table" do
       cell_text = find("tr:nth-child(1) td:nth-child(3)").text
       assert_equal "08:30, G2, 09-27-25", cell_text
+
+      cell_text = find("tr:nth-child(2) td:nth-child(3)").text
+      assert_equal "09:00, G2, 09-27-25", cell_text
+
+      cell_text = find("tr:nth-child(3) td:nth-child(3)").text
+      assert_equal "09:30, G2, 09-27-25", cell_text
+
+      cell_text = find("tr:nth-child(4) td:nth-child(3)").text
+      assert_equal "10:00, G2, 09-27-25", cell_text
+
+      cell_text = find("tr:nth-child(5) td:nth-child(3)").text
+      assert_equal "10:30, G2, 09-27-25", cell_text
+
+      cell_text = find("tr:nth-child(6) td:nth-child(3)").text
+      assert_equal "11:00, G2, 09-27-25", cell_text
     end
    
   end
