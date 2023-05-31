65005247e227418551892a4d41777b3fb8072cad
Kevin Williams <kjoewill@gmail.com>
2023-05-31 15:03:46 -0600

    Refactoring home controller and view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     params.permit(:datepicker)
     @selected_date = (params[:datepicker] || Date.today).to_date
-    @day_schedules = DaySchedule.includes(:reservations).where(day: @selected_date)
+    #@day_schedules = DaySchedule.includes(:reservations).where(day: @selected_date)
     @assets = Asset.order(:sort_order)
     @time_slots = Reservation::TIME_SLOTS
   end
