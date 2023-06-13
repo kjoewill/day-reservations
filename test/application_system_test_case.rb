40951c920381944dacd5411f35218bc3805f0a25
Kevin Williams <kjoewill@gmail.com>
2023-06-13 16:48:11 -0600

    Use headless Chrome for system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,7 +2,7 @@ require 'database_cleaner'
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
   setup do
     DatabaseCleaner.strategy = :truncation
     DatabaseCleaner.clean_with(:truncation)
