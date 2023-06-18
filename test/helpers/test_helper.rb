4770c2e6b63a9e020ea331b6a99e02178351534d
Kevin Williams <kjoewill@gmail.com>
2023-06-17 21:16:44 -0600

    Added test helper in support of headless chrom in Heroku test environment

---
+++ test/helpers/test_helper.rb
@@ -0,0 +1,14 @@
+#This code to provide correct path to the chrome library on heroku test environment
+chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
+
+chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
+
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(
+     app,
+     browser: :chrome,
+     desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
+  )
+end
+
+Capybara.javascript_driver = :chrome
