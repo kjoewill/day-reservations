55750877be3960918fb08a0ea71da4aa409e508b
Kevin Williams <kjoewill@gmail.com>
2023-06-18 11:11:17 -0600

    Added some logging to the test helper

--- test/helpers/test_helper.rb
+++ test/helpers/test_helper.rb
@@ -1,7 +1,9 @@
 #This code to provide correct path to the chrome library on heroku test environment
 chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
-
 chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
+
+Rails.logger.info("Chrome binary: #{chrome_bin}")
+Rails.logger.info("Chrome options: #{chrome_opts.inspect}")
 
 Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(
