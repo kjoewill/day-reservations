55b2f5e489d63a5bca3003a95ee4e352eec7b130
Kevin Williams <kjoewill@gmail.com>
2023-06-18 11:55:30 -0600

    Added chrome lib path set up directly to test/test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,26 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  #This code to provide correct path to the chrome library on heroku test environment
+  chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
+  chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
+
+  Rails.logger.info("Chrome binary: #{chrome_bin}")
+  Rails.logger.info("Chrome options: #{chrome_opts.inspect}")
+
+  Capybara.register_driver :chrome do |app|
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :chrome,
+      desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
+    )
+  end
+
+  Capybara.javascript_driver = :chrome
+  # End chrome lib path setup
+
+
+
+
 end
