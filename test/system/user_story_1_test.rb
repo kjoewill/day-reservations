2b957c59e229b03a9ac0bccff6c6a9a1683bffd5
Kevin Williams <kjoewill@gmail.com>
2023-07-06 21:01:07 -0600

    Added system tests for User Story 1

---
+++ test/system/user_story_1_test.rb
@@ -0,0 +1,35 @@
+require "application_system_test_case"
+
+class UserStory1Test < ApplicationSystemTestCase
+
+  test "viewing the home page" do
+
+    visit root_path
+    assert_selector "h1", text: "High Flights Soaring", wait: 5
+
+    today = Date.today
+    formatted_date = today.strftime('%m/%d/%Y')
+    reservations_text = "Reservations for #{formatted_date}"
+    assert_text reservations_text
+
+    datepicker_value = find_field("datepicker").value
+    datepicker_string = Date.parse(datepicker_value).strftime('%m/%d/%Y')
+    assert_equal formatted_date, datepicker_string
+
+    # Verify the header row of the schedule 
+    assert_equal 'Time', find('table thead tr th:nth-child(1)').text
+    assert_equal 'Glider-1 Reserve', find('table thead tr th:nth-child(2)').text
+    assert_equal 'Glider-2 Reserve', find('table thead tr th:nth-child(3)').text
+    assert_equal 'Glider-3 Reserve', find('table thead tr th:nth-child(4)').text
+    assert_equal 'Glider-4 Reserve', find('table thead tr th:nth-child(5)').text
+
+    #verify second row
+    assert_equal '08:30', find('table tbody tr:nth-child(1) td:nth-child(1)').text
+    assert_equal '-', find('table tbody tr:nth-child(1) td:nth-child(2)').text
+    assert_equal '-', find('table tbody tr:nth-child(1) td:nth-child(3)').text
+    assert_equal '-', find('table tbody tr:nth-child(1) td:nth-child(4)').text
+    assert_equal '-', find('table tbody tr:nth-child(1) td:nth-child(5)').text
+
+  end 
+
+end
