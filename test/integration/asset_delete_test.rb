395bf7b460536fb0a4cc021a3392b26e9ec811e3
Kevin Williams <kjoewill@gmail.com>
2023-07-10 14:58:05 -0600

    Removing fixtures

--- test/integration/asset_delete_test.rb
+++ test/integration/asset_delete_test.rb
@@ -1,12 +1,30 @@
 require "test_helper"
 
 class AssetsDeleteTest < ActiveSupport::TestCase
-    include ActiveRecord::TestFixtures
-    fixtures :assets
 
-  test 'There are three Asset instances' do
+  test 'There are four Asset instances' do
     assets = Asset.all
-    assert_equal 3, assets.count
+    assert_equal 4, assets.count
   end
+
+  test 'If an asset is deleted then there are four' do
+    asset = Asset.all.first
+    asset.destroy
+    assert_equal 3, Asset.all.count
+  end
+
+  test 'There are always four Asset instances at the begining of a test' do
+    assets = Asset.all
+    assert_equal 4, assets.count
+  end
+
+  test 'Glider-2 has one daySchedule and six reservations' do
+    Asset.all.each do |asset|
+        puts asset.name
+    end
+
+    # g2 = Asset.find_by(name: 'Glider-2')
+    # assert_equal 1, g2.day_schedules.count
+  end 
 
 end
--- test/integration/asset_test.rb
+++ test/integration/asset_test.rb
@@ -1,34 +1,10 @@
 require "test_helper"
 
 class AssetsPriorityTest < ActiveSupport::TestCase
-    include ActiveRecord::TestFixtures
-    fixtures :assets
 
-  test 'There are three Asset instances' do
+  test 'There are 4 Asset instances' do
     assets = Asset.all
-    assert_equal 3, assets.count
+    assert_equal 4, assets.count
   end
   
-  test 'Assets can be retreived in priority order' do
-
-    assets = Asset.by_sort_order
-    assert_equal 'FancyGlider3', assets.first.name
-    assert_equal 'FancyGlider1', assets.last.name
-
-  end
-
-  test 'Priority order can be changed' do
-
-    assets = Asset.by_sort_order
-    assert_equal 'FancyGlider3', assets.first.name
-
-    assets.first.sort_order = 99
-    assets.first.save
-    assets = Asset.by_sort_order
-
-    assert_equal 'FancyGlider2', assets.first.name
-    assert_equal 'FancyGlider3', assets.last.name
-
-  end
-
 end
