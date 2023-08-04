0abdf9a5a630f8db9a49121e21126c3240ba8f3c
Kevin Williams <kjoewill@gmail.com>
2023-08-04 17:19:29 -0600

    Modified time range for production

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,5 @@
 class Reservation < ApplicationRecord
-  TIME_SLOTS = %w[08:30 09:00 09:30 10:00 10:30 11:00]
+  TIME_SLOTS = %w[08:00 08:30 09:00 09:30 10:00 10:30 11:00 11:30 12:00 12:30 13:00 13:30 14:00 14:30 15:00 15:30 16:00 16:30]
 
   belongs_to :day_schedule
   
